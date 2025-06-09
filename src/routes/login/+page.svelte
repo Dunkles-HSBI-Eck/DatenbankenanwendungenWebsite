ddb8830a0b516c24c92ffd2f4c711f95788a8910
EliasKehne <welt.schlange@gmail.com>
2025-06-09 19:47:35 +0200

    Add Login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { goto } from '$app/navigation';
+    import { ArrowLeft, JapaneseYenIcon } from '@lucide/svelte';
+	import { json } from '@sveltejs/kit';
+
+    let email
+    let password
+
+
+    async function login() {
+        const respond = await fetch("/api/v1/login", {
+            method:"POST",
+            body:JSON.stringify({
+                email,
+                password
+            })
+        })
+
+        if (respond.ok) {
+            goto("/")
+        }
+        
+        if (!respond.ok) {
+            console.log("not found")
+        }
+    }
+</script>
+
+<main class="flex items-center justify-center min-h-screen bg-surface-950 relative">
+    <a href="/" class="absolute top-6 left-6 flex items-center gap-2 text-secondary-400">
+        <ArrowLeft class="w-7 h-7 p-1 rounded hover:bg-surface-800" />
+    </a>
+    <form class="p-10 rounded-2xl w-full max-w-md flex flex-col gap-6 shadow-2xl bg-surface-900 border border-surface-800 backdrop-blur-md">
+        <h2 class="text-3xl font-extrabold text-center mb-4 text-secondary-400 drop-shadow">Login</h2>
+        <label class="flex flex-col gap-2 rounded-xl p-2">
+            <span class="font-medium text-secondary-400">E-mail</span>
+            <input
+                bind:value={email}
+                class="input input-bordered input-lg w-full text-secondary-400 bg-surface-800 border border-surface-700 focus:ring-primary-400 focus:outline-none rounded-lg"
+                type="email"
+                placeholder="E-mail"
+                required/>
+        </label>
+        <label class="flex flex-col gap-2 rounded-xl p-2">
+            <span class="font-medium text-secondary-400">Password</span>
+            <input
+                bind:value={password}
+                type="password"
+                placeholder="Password"
+                class="input input-bordered input-lg w-full text-secondary-400 bg-surface-800 border border-surface-700 focus:ring-primary-400 focus:outline-none rounded-lg"
+                required/>
+        </label>
+        <button
+            type="submit"
+            onclick={login}
+            class="btn btn-lg btn-block text-secondary-400 rounded-xl bg-primary-500 shadow-md focus:ring-2 focus:ring-secondary-300">
+            Login
+        </button>
+        <div class="text-center mt-2 text-primary-400">
+            <a href="/register" class="hover:underline hover:text-primary-200 transition-colors duration-150">Register</a>
+        </div>
+    </form>
+</main>
