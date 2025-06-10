98e944737c17065965356e394c24ebe95bb772da
Christian <Christian.dueck@gmx.net>
2025-06-10 20:41:44 +0200

    Updated Mainpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,25 @@
 <script>
-	import { Skull } from '@lucide/svelte';
+	import LogoBtn from './LogoBtn.svelte';
 </script>
 
-<h1>VintageFlix</h1>
-<img src="Logo.png" , alt="Logo" />
-<button>Login</button>
-<button>Register</button>
-<Skull />
+<nav class="sticky top-0 z-10 flex items-center justify-between mb-8 px-2 w-full bg-surface/80 backdrop-blur-md border-b border-secondary-400">
+	<!-- Left -->
+	<div class="justify-start">
+		<LogoBtn/>
+	</div>
+
+	<!-- Mid -->
+	<div>
+
+	</div>
+
+	<!-- Right -->
+	<div class="justify-end">
+		<div class="flex gap-1">
+			<a href="/login" class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 focus:ring-2 rounded-xl shadow-md hover:scale-105">Login</a>
+			<a href="/register" class="btn btn-lg btn-block text-secondary-400 bg-transparent focus:bg-primary-500 hover:ring-secondary-300 hover:ring-2 hover:scale-105 rounded-xl shadow-md">Register</a>
+		</div>
+	</div>
+</nav>
+
+<img src="LogoCropped.png" alt="Logo"/>
---
+++ src/routes/LogoBtn.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<a href="/">
+	<div class="bg-secondary-500 m-3 p-2 duration-100 hover:scale-105 rounded-2xl">
+		<img src="LogoCropped.png" alt="Logo" class="h-10 w-10" />
+	</div>
+</a>
