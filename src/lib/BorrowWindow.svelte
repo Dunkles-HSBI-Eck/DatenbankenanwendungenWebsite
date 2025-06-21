5c496df5f9d4ca612e51aa90d97135a4e0bee49d
Julius Reuter <jureuter06@gmail.com>
2025-06-21 19:36:05 +0200

    reworked window

--- src/lib/BorrowWindow.svelte
+++ src/lib/BorrowWindow.svelte
@@ -1,33 +1,23 @@
 <script>
 	import { X as XIcon } from '@lucide/svelte';
-
+  import { bounceOut, cubicOut, sineOut } from 'svelte/easing';
+  import { fly } from 'svelte/transition';
 	let { movieTitle, cancelFunction, price } = $props();
 </script>
 
-<div class="fixed z-50 flex h-full w-full items-center justify-center text-secondary-500">
-	<div
-		class="bg-surface-800 border-surface-700 flex h-1/2 w-1/2 flex-col rounded border shadow-2xl"
+<div class="fixed z-50 flex h-full w-full items-center justify-center text-secondary-500 pt-60">
+  <div
+		class="bg-surface-800 border-surface-700 flex h-full w-1/2 flex-col rounded border shadow-2xl" in:fly={{duration:500, easing:cubicOut, y:1000, opacity:100}}
 	>
-		<!-- Top bar -->
-		<div class="bg-surface-900 flex h-10 w-full items-center justify-between">
-			<h1 class="p-4">rent Movie: {movieTitle}</h1>
-			<button
-				onclick={cancelFunction}
-				class="hover:bg-primary-500 flex h-full w-fit items-center rounded"
-			>
-				<XIcon class="m-6 h-5 w-6 cursor-pointer text-secondary-500" />
-			</button>
-		</div>
-
 		<!-- Content -->
-		<div class="flex-grow p-4">
-			<p>You are about to rent the following movie for 14 days for a price of {price || 0}€:</p> <br>
-      <p>{movieTitle}</p>
-      
+		<div class=" p-4">
+			<p>You are about to rent the following movie for 14 days for a price of {price || 0}€: </p> <br>
+      <div class="flex p-4 " > <p>{movieTitle}</p></div>
+
 		</div>
 
 		<!-- Bottom buttons -->
-		<div class="border-surface-700 flex w-full justify-between border-t px-10 py-4">
+		<div class="border-surface-700 flex w-full justify-between border-t px-10 py-10 mt-10">
 			<button
 				onclick={cancelFunction}
 				class="bg-surface-700 hover:bg-surface-600 flex rounded px-4 py-2 ">Cancel</button
