efde427a61cd8c49ae8fe005eb53a3840a79bebe
Julius Reuter <jureuter06@gmail.com>
2025-06-21 15:03:04 +0200

    remove unneccesary console.log

--- src/routes/movie/[id]/+page.js
+++ src/routes/movie/[id]/+page.js
@@ -1,10 +1,8 @@
 
 export async function load({ params, fetch }) {
-    console.log("1st request");
     const response = await fetch(`/api/v1/movies/${params.id}`);
     const movie = await response.json();
     let ownsMovie = false;
-    console.log("2nd request");
     const response2 = await fetch('/api/v1/borrowedMovies');
 		const data = await response2.json();
 		// Map API data to table format
