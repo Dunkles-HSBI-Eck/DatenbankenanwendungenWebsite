5296c11b5af7d40b01049fa117ea32a562dbb2b4
Julius Reuter <jureuter06@gmail.com>
2025-06-23 19:36:06 +0200

    format code

--- src/lib/BorrowWindow.svelte
+++ src/lib/BorrowWindow.svelte
@@ -1,30 +1,29 @@
 <script>
 	import { X as XIcon } from '@lucide/svelte';
-  import { bounceOut, cubicOut, sineOut } from 'svelte/easing';
-  import { fly } from 'svelte/transition';
+	import { bounceOut, cubicOut, sineOut } from 'svelte/easing';
+	import { fly } from 'svelte/transition';
 	let { movieTitle, cancelFunction, price } = $props();
 </script>
 
-<div class="fixed z-50 flex h-full w-full items-center justify-center text-secondary-500 pt-60">
-  <div
-		class="bg-surface-800 border-surface-700 flex h-full w-1/2 flex-col rounded border shadow-2xl" in:fly={{duration:500, easing:cubicOut, y:1000, opacity:100}}
+<div class="text-secondary-500 fixed z-50 flex h-full w-full items-center justify-center pt-60">
+	<div
+		class="bg-surface-800 border-surface-700 flex h-full w-1/2 flex-col rounded border shadow-2xl"
+		in:fly={{ duration: 500, easing: cubicOut, y: 1000, opacity: 100 }}
 	>
 		<!-- Content -->
 		<div class=" p-4">
-			<p>You are about to rent the following movie for 14 days for a price of {price || 0}€: </p> <br>
-      <div class="flex p-4 " > <p>{movieTitle}</p></div>
-
+			<p>You are about to rent the following movie for 14 days for a price of {price || 0}€:</p>
+			<br />
+			<div class="flex p-4"><p>{movieTitle}</p></div>
 		</div>
 
 		<!-- Bottom buttons -->
-		<div class="border-surface-700 flex w-full justify-between border-t px-10 py-10 mt-10">
+		<div class="border-surface-700 mt-10 flex w-full justify-between border-t px-10 py-10">
 			<button
 				onclick={cancelFunction}
-				class="bg-surface-700 hover:bg-surface-600 flex rounded px-4 py-2 ">Cancel</button
+				class="bg-surface-700 hover:bg-surface-600 flex rounded px-4 py-2">Cancel</button
 			>
-			<button class="bg-primary-500 hover:bg-primary-600 flex rounded px-4 py-2 "
-				>Confirm</button
-			>
+			<button class="bg-primary-500 hover:bg-primary-600 flex rounded px-4 py-2">Confirm</button>
 		</div>
 	</div>
 </div>
--- src/routes/(app)/movie/[id]/+page.svelte
+++ src/routes/(app)/movie/[id]/+page.svelte
@@ -49,18 +49,21 @@
 </script>
 
 {#if showBorrowWindow}
-	<BorrowWindow movieTitle={movie.title} cancelFunction={cancelBorrowing} price = {movie.price}/>
+	<BorrowWindow movieTitle={movie.title} cancelFunction={cancelBorrowing} price={movie.price} />
 {/if}
 
+<div class=" -z-30 -mt-12 h-[50rem] w-screen overflow-hidden">
+	<img
+		src="/api/v1/images/banners/{movie.banner}"
+		alt="thumbnail"
+		class="w-full object-cover brightness-75"
+	/>
+</div>
 
-	<div class=" -z-30 w-screen  overflow-hidden -mt-12 h-[50rem]">
-		<img src="/api/v1/images/banners/{movie.banner}" alt="thumbnail" class="w-full object-cover brightness-75" />
-	</div>
-
-
-	<div class=" bottom-0 bottom h-96 bg-surface-900 border-surface-800 z-40 flex w-full rounded border p-10 relative">
-
-<div class=" z-40 flex w-250 pt-20 pl-10 absolute -left-10 bottom-[26.5rem]">
+<div
+	class=" bottom bg-surface-900 border-surface-800 relative bottom-0 z-40 flex h-96 w-full rounded border p-10"
+>
+	<div class=" absolute bottom-[26.5rem] -left-10 z-40 flex w-250 pt-20 pl-10">
 		<div
 			bind:this={focusOnLoad}
 			class="bg-surface-900 border-surface-800 shadow-surface-950 w-full items-baseline rounded border p-10 shadow-2xl"
@@ -78,68 +81,68 @@
 		</div>
 	</div>
 
-		<div class="text-secondary-500 mr-10 h-full">
-			<p class="">licences avalable: {movie.available_licenses}</p>
-			<p>movie length: {formatTime(movie.length)}</p>
-			<br />
-			{#each cast as category}
-				<p>
-					<strong>{category.task}</strong>:
-					<br />
-					{#each category.people as castmember}
-						{castmember.name} <br />
-					{/each}
-					<br />
-				</p>
-			{/each}
-		</div>
-		{#if isLoggedIn}
-			{#if userOwnsMovie}
-				<a href="/watch"
-					><div
-						class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 h-27 rounded-xl shadow-md focus:ring-2"
-					>
-						<p class="flex">Watch now</p>
-						<TvMinimalPlay class="flex" />
-					</div></a
-				>
-				<button
-					class="btn btn-lg btn-block text-secondary-400 bg-surface-900 focus:ring-secondary-300 ml-10 h-27 rounded-xl shadow-md transition-colors duration-150 hover:underline focus:ring-2"
-				>
-					<p class="flex">return movie</p>
-					<ReturnIcon class="flex" />
-				</button>
-			{:else if movie.available_licenses > 0}
-				<button
-					onclick={openBorrowWindow}
-					class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 h-27 rounded-xl shadow-md focus:ring-2"
-				>
-					<p>rent movie for 14 days</p>
-					<p>for {movie.price}€</p>
-				</button>
-			{:else}
-				<button
-					class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 h-27 rounded-xl shadow-md focus:ring-2"
-				>
-					<p>make a reservation</p>
-				</button>
-			{/if}
-		{:else}
-			<div class="text-secondary-500 p-10">
-				<p class="text-secondary-500">
-					To watch this movie please log in to your account or create a new one
-				</p>
+	<div class="text-secondary-500 mr-10 h-full text-xs">
+		<p class="">licences avalable: {movie.available_licenses}</p>
+		<p>movie length: {formatTime(movie.length)}</p>
+		<br />
+		{#each cast as category}
+			<p>
+				<strong>{category.task}</strong>:
 				<br />
-				<div class="flex w-[12.5rem] justify-end gap-1">
-					<a
-						href="/register"
-						class="btn btn-lg hover:ring-secondary-300 h-12 w-24 rounded-lg hover:scale-105"
-						>Register</a
-					>
-					<a href="/login" class="btn btn-lg bg-primary-500 h-12 w-24 rounded-lg hover:scale-105"
-						>Login</a
-					>
-				</div>
-			</div>
-		{/if}
+				{#each category.people as castmember}
+					{castmember.name} <br />
+				{/each}
+				<br />
+			</p>
+		{/each}
 	</div>
+	{#if isLoggedIn}
+		{#if userOwnsMovie}
+			<a href="/watch"
+				><div
+					class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 h-27 rounded-xl shadow-md focus:ring-2"
+				>
+					<p class="flex">Watch now</p>
+					<TvMinimalPlay class="flex" />
+				</div></a
+			>
+			<button
+				class="btn btn-lg btn-block text-secondary-400 bg-surface-900 focus:ring-secondary-300 ml-10 h-27 rounded-xl shadow-md transition-colors duration-150 hover:underline focus:ring-2"
+			>
+				<p class="flex">return movie</p>
+				<ReturnIcon class="flex" />
+			</button>
+		{:else if movie.available_licenses > 0}
+			<button
+				onclick={openBorrowWindow}
+				class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 h-27 rounded-xl shadow-md focus:ring-2"
+			>
+				<p>rent movie for 14 days</p>
+				<p>for {movie.price}€</p>
+			</button>
+		{:else}
+			<button
+				class="btn btn-lg btn-block text-secondary-400 bg-primary-500 focus:ring-secondary-300 h-27 rounded-xl shadow-md focus:ring-2"
+			>
+				<p>make a reservation</p>
+			</button>
+		{/if}
+	{:else}
+		<div class="text-secondary-500 p-10">
+			<p class="text-secondary-500">
+				To watch this movie please log in to your account or create a new one
+			</p>
+			<br />
+			<div class="flex w-[12.5rem] justify-end gap-1">
+				<a
+					href="/register"
+					class="btn btn-lg hover:ring-secondary-300 h-12 w-24 rounded-lg hover:scale-105"
+					>Register</a
+				>
+				<a href="/login" class="btn btn-lg bg-primary-500 h-12 w-24 rounded-lg hover:scale-105"
+					>Login</a
+				>
+			</div>
+		</div>
+	{/if}
+</div>
