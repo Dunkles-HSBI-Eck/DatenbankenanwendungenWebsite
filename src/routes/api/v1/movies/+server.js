7e07d4edd99364c48dc65721074ce48357c6d0a8
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2025-06-27 10:16:39 +0200

    Fix get movies endpoint

--- src/routes/api/v1/movies/+server.js
+++ src/routes/api/v1/movies/+server.js
@@ -2,7 +2,7 @@ import { json } from '@sveltejs/kit';
 import { getMovies } from '$lib/server/database.js';
 
 export async function GET({ url }) {
-	const last_id = parseInt(url.searchParams.get('last_id')) || -1;
+	let last_id = url.searchParams.get('last_id') || null;
 	const page_size = Math.min(parseInt(url.searchParams.get('page_size')) || 10, 100);
     const search = url.searchParams.get('search') || '';
     const order_by = url.searchParams.get('order_by') || 'id';
@@ -25,14 +25,13 @@ export async function GET({ url }) {
         }
     }
 
-
 	// Get the movies from the database
-	const result = await getMovies(last_id, page_size, search, order_by, order, genres);
+	const result = await getMovies(last_id, page_size, search, order_by, order, JSON.stringify(genres));
 
     let resultLastId = null;
     switch (order_by) {
         case 'release':
-            resultLastId = result.movies?.length ? result.movies[result.movies.length - 1].release : null;
+            resultLastId = result.movies?.length ? result.movies[result.movies.length - 1].release : null; 
             break;
         case 'alphabetical':
             resultLastId = result.movies?.length ? result.movies[result.movies.length - 1].name : null;
