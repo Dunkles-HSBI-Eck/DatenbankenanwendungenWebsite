06f046fe89b42b95cd36f20755ad42064657ab0a
EliasKehne <welt.schlange@gmail.com>
2025-06-28 16:11:07 +0200

    remove unused features from the profile page

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -37,8 +37,6 @@
 			titel: reservation.title,
 			status: reservation.status
 		}));
-
-		console.log(data2);
 	});
 
 	async function logout() {
@@ -63,9 +61,6 @@
 			<span class="text-secondary-400 font-semibold">Current Plan:</span>
 			<span class="text-tertiary-500">{userTier}</span>
 			<div class="mt-4 flex flex-col gap-2">
-				<a class="btn bg-primary-500 text-secondary-400 self-start" href="/payment">
-					Add Payment Option
-				</a>
 				<a class="btn bg-primary-500 text-secondary-400 self-start" href="/subscriptions">
 					Change plan
 				</a>
@@ -88,38 +83,7 @@
 			</div>
 		</div>
 		<div class="card bg-surface-950 border-surface-200-800 mb-2 flex-1 border-[1px] p-6">
-			<form class="mb-2 flex w-full flex-col gap-2" style="max-width: 320px;">
-				<label class="text-secondary-400 text-sm font-semibold" for="current-password"
-					>Current Password</label
-				>
-				<input
-					id="current-password"
-					type="password"
-					class="input input-sm bg-surface-800 border-surface-700 rounded border px-2 py-1 focus:outline-none"
-					autocomplete="current-password"
-				/>
-				<label class="text-secondary-400 text-sm font-semibold" for="new-password"
-					>New Password</label
-				>
-				<input
-					id="new-password"
-					type="password"
-					class="input input-sm bg-surface-800 border-surface-700 rounded border px-2 py-1 focus:outline-none"
-					autocomplete="new-password"
-				/>
-				<label class="text-secondary-400 text-sm font-semibold" for="confirm-password"
-					>Confirm New Password</label
-				>
-				<input
-					id="confirm-password"
-					type="password"
-					class="input input-sm bg-surface-800 border-surface-700 rounded border px-2 py-1 focus:outline-none"
-					autocomplete="new-password"
-				/>
-				<button type="button" class="btn btn-xs bg-primary-500 text-secondary-400 mt-2 self-end"
-					>Change Password</button
-				>
-			</form>
+			<WishMovie />
 		</div>
 	</div>
 	<div class="flex w-full max-w-4xl flex-col justify-center gap-6 md:flex-row">
@@ -129,8 +93,5 @@
 		<div class="card bg-surface-950 border-surface-200-800 flex-1 border-[1px] p-8">
 			<Reserved tableData={ReservedTableData} />
 		</div>
-	</div>
-	<div class="card bg-surface-950 border-surface-200-800 flex-1 border-[1px] p-8">
-		<WishMovie />
 	</div>
 </div>
