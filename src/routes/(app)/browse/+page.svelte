417c0a82f12fb87e666afbb0efac23daccef4dc5
Hicks-99 <94490389+Hicks-99@users.noreply.github.com>
2025-06-30 17:57:40 +0200

    Add request link on browse page linking profile page

--- src/routes/(app)/browse/+page.svelte
+++ src/routes/(app)/browse/+page.svelte
@@ -242,5 +242,12 @@
 		>
 			Mehr Filme laden
 		</button>
+    {:else}
+		<div class="text-secondary-600 space-y-2 text-center">
+			<p>Nicht gefunden wonach du suchst?</p>
+			<a href="/profile" class="btn bg-primary-500 rounded-lg px-4 py-2 hover:scale-105"
+				>Schlag uns einen Film vor!</a
+			>
+		</div>
 	{/if}
 </div>
